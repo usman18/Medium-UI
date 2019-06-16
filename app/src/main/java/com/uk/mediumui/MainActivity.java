8fd46c91d41e05c5eeeb0b54d456f1174a17f5ec
usman18 <secretdeveloper18@gmail.com>
2019-06-17 01:22:41 +0530

    -> Initial setup(font, colors, nav drawer, toolbar) done.

--- app/src/main/java/com/uk/mediumui/MainActivity.java
+++ app/src/main/java/com/uk/mediumui/MainActivity.java
@@ -1,13 +1,56 @@
 package com.uk.mediumui;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+	
+	private DrawerLayout drawerLayout;
+	private NavigationView navigationView;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+		
+		toolbar.setTitle("Home");
+		
+		drawerLayout = findViewById(R.id.drawer);
+		navigationView = findViewById(R.id.navigationView);
+		
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_screen_options, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				drawerLayout.openDrawer(Gravity.START);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
